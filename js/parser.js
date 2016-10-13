c140e203b8ee7d1f2fd0fcb9917734c19e664728
makahoshi <mkakahoshi@gmail.com>
2016-10-12 22:56:22 -0400

    moved js to new file and also added jquery

---
+++ js/parser.js
@@ -0,0 +1,73 @@
+var start,
+    finish,
+    gridwidth;
+var objects = [];
+var openFile = function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function() {
+        var text = reader.result;
+        //console.log(typeof reader.result); double checking that this is a string
+        //now with our text we need to parse the string
+        var new_text = text.split(/[()]+/); //this splits the strings by the parentheses
+        console.table(new_text);
+        var size = printGrid(new_text);
+        var startPos = findStart(new_text);
+        /*in this part of the parsing I am splicing the array at the completed text, since all of the level_design coordinates listed for the wall, touch, gem and altar are the same as the previous coordinates. This will prevent any redrawing of a tile*/
+        var splicelocation = new_text.indexOf(' completed __level_design');
+        var newText = new_text.splice(0, splicelocation);
+        /* there are 52 objects being created with the coordinates and the type of sprite*/
+        function tiles(coordinates, touch, sprite) {
+            this.coordinates = coordinates; //it is the coordinate
+            this.touch = touch;
+            this.sprite = sprite; //can be a wall, altar, gem, or the default tile
+        }
+        var counter = 0;
+        for (var i = 0; i < newText.length; i++) {
+            if (newText[i] === ",wall") {
+                counter++; //increment the counter
+                var j = i - 1; //subtracting 1 because the coordinate location in the array is before the wall
+                var coor = newText[j];
+                window["Object" + counter] = new tiles(coor,"null", "wall"); //creating the object
+                objects.push(eval("Object" + counter));
+            } else if (newText[i] === ",altar") {
+                counter++; //increment the counter
+                var j = i - 1; //subtracting 1 because the coordinate location in the array is before the altar
+                var coor = newText[j];
+                window["Object" + counter] = new tiles(coor,"null", "altar"); //creating the object
+                objects.push(eval("Object" + counter));
+            } else if (newText[i] === ",gem") {
+                counter++; //increment the counter
+                var j = i - 1; //subtracting 1 because the coordinate location in the array is before the gem
+                var coor = newText[j];
+                window["Object" + counter] = new tiles(coor,"null", "gem"); //creating the object
+                objects.push(eval("Object" + counter));
+            } else {
+                continue;
+            }
+        }
+        createBoard(size, startPos[0], startPos[1]);
+        for (var i = 0; i < objects.length; i++) {
+            setTile(size, objects[i]);
+        }
+    }
+    reader.readAsText(input.files[0]);
+};
+var findStart = function(x) {
+    var startLocation = x.indexOf(' start') + 1; //this is finding the position of where 'start' is
+    var finishLocation = x.indexOf(' finish') + 1; //this is finding the position of where 'finish' is
+    start = x[startLocation]; //this is the start coordinates
+    finish = x[finishLocation]; //this is the end coordinates
+    startEnd = [start, finish];
+    return startEnd;
+}
+var printGrid = function(x) {
+    var width = x[1];
+    var size = width.split(",");
+    gridwidth = size[1]; //this is the width of the grid
+    return gridwidth; //now we know the width of the grid and assuming that it will always be a square we know that the height is also the same as the width
+}
+var createBoard = function(total, sPos, fPos) {
+    createRectangles(total * total);
+    setSprite(total, sPos, fPos);
+}
