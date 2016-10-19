2d1d4d639fac07c4f9c471c112e79d48771fe2cf
makahoshi <mkakahoshi@gmail.com>
2016-10-19 18:05:11 -0400

    configured the regular tiles when a user mouse over those, need to still find a way for current tile thoug

--- play.js
+++ play.js
@@ -8,56 +8,6 @@ function lose(event) {
         wall[i].classList.add("wallMouse");
     }
 }
-//Calling this function when the user mouseovers the regular free tiles
-//currently this is not working
-function regular(event) {
-    console.log("you cannot walk through walls");
-    loser = true;
-    var tile = document.querySelectorAll(".free ");
-    for (var i = 0; i < tile.length; i++) {
-        tile[i].classList.remove("tile");
-        tile[i].classList.add("walking");
-        console.log("just walking the game");
-    }
-}
-
-//calling this function when the user wins the game and makes it to the finish tile
-function win(event) {
-    time_end = new Date();
-    alert("You Win");
-    return time_end;
-}
-
-//Calling this function when the user leaves the game area
-function endGame(event) {
-    time_end = new Date();
-    alert("You left the game and are out of bounds");
-    return time_end;
-}
-
-//Calling this function when the user first hovers over the start
-function startGame(event) {
-    //timer function begins
-    time_start = new Date();
-    var start = document.querySelectorAll(".start");
-    //console.log(start);
-    start[0].classList.remove("start");
-    start[0].classList.add("startMouse");
-    var finish = document.querySelectorAll(".finish");
-    var wall = document.querySelectorAll(".wall");
-    for (var i = 0; i < wall.length; i++) {
-        wall[i].onmouseover = lose;
-        wall[i].onmouseleave = leaveWall;
-    }
-    return time_start;
-}
-
-//Calling this function when the user is not hovering over the start
-function continueGame(event) {
-    var tiles = document.getElementById("game").children;
-    tiles[0].classList.remove("startMouse");
-    tiles[0].classList.add("start");
-}
 
 //Calling this function when the user is not hovering over the walls
 function leaveWall(event) {
@@ -72,6 +22,102 @@ function leaveWall(event) {
         wall_array[i].classList.remove("wallMouse");
         wall_array[i].classList.add("wall");
     }
+}
+
+//Calling this function when the user mouseovers the regular free tiles
+function walk(event) {
+  console.log("Walking");
+    var tile_array =[];
+    var tile = document.querySelectorAll(".tile");
+    for(var i=0; i<tile.length; i++){
+      if (tile[i].className == "tile "){
+        tile_array.push(tile[i]);
+      }
+    }
+    for (var i = 0; i < tile_array.length; i++) {
+      //check if mouse on current i
+      var x = event.clientX;
+      var y = event.clientY;
+      tile_array[i].classList.add("tileMouse");
+    }
+}
+
+function leaveTile(event) {
+    var tile_array =[];
+    var bool = true;
+    var tile = document.getElementById("game").children;
+    console.log(tile);
+    for(var i=0; i<tile.length; i++){
+      if (tile[i].className == "tile tileMouse"){
+        tile_array.push(tile[i]);
+      }
+      else if (tile[i].className == "tile tileLeave"){
+        tile_array.push(tile[i]);
+        bool = false
+      }
+    }
+    for (var i = 0; i < tile_array.length; i++){
+      if (bool === true){
+        tile_array[i].classList.remove("tileMouse");
+        tile_array[i].classList.add("tileLeave");
+      }
+      else{
+        tile_array[i].classList.remove("tileLeave");
+        tile_array[i].classList.add("tileMouse");
+      }
+    }
+}
+
+//calling this function when the user wins the game and makes it to the finish tile
+function win(event) {
+    time_end = new Date();
+    //alert("You Win");
+    return time_end;
+}
+
+//Calling this function when the user leaves the game area
+function endGame(event) {
+    time_end = new Date();
+    //alert("You left the game and are out of bounds");
+    return time_end;
+}
+
+//Calling this function when the user first hovers over the start
+function startGame(event) {
+    //timer function begins
+    time_start = new Date();
+    var start = document.querySelectorAll(".start");
+    var tile_array =[];
+    //console.log(start);
+    start[0].classList.remove("start");
+    start[0].classList.add("startMouse");
+    //if the user walks over a wall
+    var wall = document.querySelectorAll(".wall");
+    for (var i = 0; i < wall.length; i++) {
+        wall[i].onmouseover = lose;
+        wall[i].onmouseleave = leaveWall;
+    }
+    //if a user walks over a regular tile
+    var tile = document.querySelectorAll(".tile");
+    //console.log(tile);
+    for(var i=0; i<tile.length; i++){
+      if (tile[i].className == "tile "){
+        //console.log(tile[i]);
+        tile_array.push(tile[i]);
+      }
+    }
+    for (var i = 0; i < tile_array.length; i++) {
+        tile_array[i].onmouseover = walk;
+      //  tile_array[i].onmouseleave = leaveTile;
+    }
+    return time_start;
+}
+
+//Calling this function when the user is not hovering over the start
+function continueGame(event) {
+    var tiles = document.getElementById("game").children;
+    tiles[0].classList.remove("startMouse");
+    tiles[0].classList.add("start");
 }
 
 //This function will be called when the user picks up the item
@@ -92,7 +138,13 @@ function dropoffItem(event) {
 
 function play() {
     var tiles = document.getElementById("game").children;
+  //  console.log(tiles);
     var tile = document.querySelectorAll(".tile");
+    console.log(tile);
+    for(var i=0; i<tile.length; i++){
+      if (tile[i].className == "tile "){
+      }
+    }
     var wall = document.querySelectorAll(".wall");
     var gem = document.querySelectorAll(".gem");
     var altar = document.querySelectorAll(".altar");
