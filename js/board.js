bdf5bbbfef32b993713499bd40a411c29b64a31b
Natalie Forman <natalie.forman@gmail.com>
2016-10-26 12:18:55 -0400

    Comment board functions

--- js/board.js
+++ js/board.js
@@ -1,6 +1,38 @@
-var createBoard = function(total, sPos, fPos) {
-    createRectangles(total * total);
-    setEnds(total, sPos, fPos);
+/*
+Find the start and end position
+@param {array} elements - The levels elements
+returns {array} startEnd - the start and end coordinates
+ */
+var findStart = function(elements) {
+    var startLocation = elements.indexOf(' start') + 1; //this is finding the position of where 'start' is
+    var finishLocation = elements.indexOf(' finish') + 1; //this is finding the position of where 'finish' is
+    start = elements[startLocation]; //this is the start coordinates
+    finish = elements[finishLocation]; //this is the end coordinates
+    startEnd = [start, finish];
+    return startEnd;
+}
+/*
+Find the width and size (since the grid is square)
+@param {array} elements - The levels elements
+returns {int} gridwidth - the width of the grid
+ */
+var calcWidth = function(elements) {
+    var width = elements[1];
+    var size = width.split(",");
+    var gridwidth = size[1]; //this is the width of the grid
+    return gridwidth; //now we know the width of the grid and assuming that it will always be a square we know that the height is also the same as the width
+}
+/*
+Create the board
+@param {int} width - The width of the board
+@param {string} sPos - The total number of tiles
+@param {string} fPos - The total number of tiles
+ */
+var createBoard = function(width, sPos, fPos) {
+	//create either 100 or 225 tiles
+    createRectangles(width * width);
+    //set the start and end tiles
+    setEnds(width, sPos, fPos);
 }
 
 /*
@@ -29,8 +61,8 @@ function createRectangles(total) {
 /*
 Set the start and end tile
 @param {int} width - The x width of the board
-@param {int} startPos - The position of start
-@param {int} endPos - The position of end
+@param {string} startPos - The position of start
+@param {string} endPos - The position of end
  */
 function setEnds(width, startPos, finishPos){
 	//find the start and end coordinates
@@ -71,19 +103,4 @@ function setTile(width, tileObj){
 	}
 	//set the selected div's class to the sprite type
 	startTile.className += tileObj.sprite;
-}
-
-var findStart = function(x) {
-    var startLocation = x.indexOf(' start') + 1; //this is finding the position of where 'start' is
-    var finishLocation = x.indexOf(' finish') + 1; //this is finding the position of where 'finish' is
-    start = x[startLocation]; //this is the start coordinates
-    finish = x[finishLocation]; //this is the end coordinates
-    startEnd = [start, finish];
-    return startEnd;
-}
-var calcWidth = function(x) {
-    var width = x[1];
-    var size = width.split(",");
-    var gridwidth = size[1]; //this is the width of the grid
-    return gridwidth; //now we know the width of the grid and assuming that it will always be a square we know that the height is also the same as the width
 }
